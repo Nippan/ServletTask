6f02861f8655b56ce9d6cfa0cccea9f2eeb0c30a
Nippan <ars6155@gmail.com>
2019-11-25 16:30:05 +0300

    First commit

---
+++ SpringRestContr/src/main/java/org/applic_spring/repos/UserRepo.java
@@ -0,0 +1,11 @@
+package org.applic_spring.repos;
+
+import org.applic_spring.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
+
